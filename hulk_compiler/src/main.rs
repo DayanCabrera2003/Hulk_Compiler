0a6cfd271402e60ed00231c6ecb530aae085029b
DayanCabrera2003 <dayancorvo@gmail.com>
2026-02-17 18:14:34 -0500

    Implement main function to analyze source code

--- hulk_compiler/src/main.rs
+++ hulk_compiler/src/main.rs
@@ -1,4 +1,11 @@
+use logos::Logos;
+mod lexer;
+mod analyze;
+use lexer::tokens::Token;
+use analyze::analyze_source;
 
-fn main(){
+fn main() {
+    let source = "";
 
+    analyze_source(source);
 }
